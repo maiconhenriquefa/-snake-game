a3cfb8c314275cc51d9a78ff5edd375d53cd1252
maiconhenriquefa <maiconhenriquefa@gmail.com>
2021-09-08 14:53:00 -0300

    alterações no style e direction

---
+++ js/script.js
@@ -0,0 +1,61 @@
+let canvas = document.getElementById("snake"); //ID do elemento que irá rodar o jogo
+let context = canvas.getContext("2d");
+let box = 32;
+let snake = [];
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
+}
+
+let direction = "left";
+
+function criarBG() {
+    context.fillStyle = "#158463";
+    context.fillRect(0, 0, 16 * box, 16 * box);
+}
+
+function criarCobrinha() {
+    for(i=0; i < snake.length; i++) {
+        context.fillStyle = "rgb(6, 248, 6)";
+        context.fillRect(snake[i].x, snake[i].y, box, box)
+    }
+}
+
+document.addEventListener('keydown', update);
+
+function update (event){
+    if(event.keyCode == 37 && direction != "right") direction = "left";
+    if(event.keyCode == 38 && direction != "down") direction = "up";
+    if(event.keyCode == 39 && direction != "left") direction = "right";
+    if(event.keyCode == 40 && direction != "up") direction = "down";    
+}
+
+function initGame(){
+    if(snake[0].x > 16 * box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+    if(snake[0].y > 16 * box && direction == "down") snake[0].y = 0;
+    if(snake[0].y < 0  && direction == "up") snake[0].y = 16 * box;
+
+
+    criarBG(); 
+    criarCobrinha();
+
+    let snakeX = snake[0].x; 
+    let snakeY = snake[0].y;
+
+    if(direction == "right") snakeX +=box;
+    if(direction == "left") snakeX -=box;
+    if(direction == "up") snakeY -=box;
+    if(direction == "down") snakeY +=box;
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY,
+    }
+
+    snake.unshift(newHead);
+}
+
+let jogo = setInterval(initGame, 100);
